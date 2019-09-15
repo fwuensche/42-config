841498fbc70f0697db71c458db82b96d889d03c0
Flavio WUENSCHE <fwuensch@e2r3p21.42.fr>
2019-09-15 12:08:53 +0200

    auto hide dock

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 read -p "Enter your 42 username: "  username
 
 # spectacle
-echo "Downloading Spectacle shortcuts file..."
+echo "Download Spectacle shortcuts file..."
 curl -sO https://raw.githubusercontent.com/fwuensche/42-config/master/Spectacle/Shortcuts.json
 echo "Moving it to Application folder..."
 mv Shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
@@ -10,17 +10,24 @@ pkill Spectacle
 open -a Spectacle
 
 # show date on status bar
-echo "Adding date to status bar..."
+echo "Add date to status bar..."
 defaults write com.apple.menuextra.clock "DateFormat" "d MMM w HH:mm" \
   && killall SystemUIServer \
   && sleep 2 && defaults read com.apple.menuextra.clock "DateFormat" >/dev/null
 
 # remap right ctrl to right alt
-echo "Remapping right-ctrl to right-alt..."
+echo "Remap right-ctrl to right-alt..."
 hidutil property --set '{"UserKeyMapping":
     [{"HIDKeyboardModifierMappingSrc":0x7000000e4,
 	      "HIDKeyboardModifierMappingDst":0x7000000e6}]
 }' >/dev/null
+
+# auto hide dock faster
+echo "Auto-hide Dock and remove its slow animation..."
+defaults write com.apple.dock autohide -bool true && \
+	defaults write com.apple.dock autohide-delay -float 0 && \
+	defaults write com.apple.dock autohide-time-modifier -float 0 && \
+	killall Dock
 
 # clean useless files
 # rm -rf ~/Library/*chache_bak*
